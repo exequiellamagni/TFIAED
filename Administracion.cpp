f4019869523ec5f33b9a52ef8ecf168f1e6ae261
exequiellamagni <Exequiel.Lamagni@alu.frt.utn.edu.ar>
2020-12-02 09:56:31 -0300

    Optimizacion de las funciones de regitro de veterinarios en Administracion.cpp

--- Administracion.cpp
+++ Administracion.cpp
@@ -18,13 +18,16 @@ struct Usuario
 };
 
 void RegistrarVet(FILE *Vet);
-int UsuarioRepetido(FILE *Vet,Usuario DatU);
-void Cargarusuario(Usuario &DatU);
+int UsuarioRepetido(char NomU[10],FILE *&Vet);
+void ComprobarUsuario(char NomU[10],int &b,FILE *&Vet);
+void ComprobarContra(char ConU[32],int &b);
 
 main()
 {
 	setlocale(LC_ALL,"spanish");
 	FILE *Vet;
+	RegistrarVet(Vet);
+	
 	
 }
 
@@ -32,7 +35,7 @@ void RegistrarVet(FILE *Vet)
 {
 	DatosVet DatV;
 	Usuario DatU;
-	
+	int b;
 	
 	Vet = fopen("Veterinario.dat","ab");
 	
@@ -49,252 +52,256 @@ void RegistrarVet(FILE *Vet)
 		gets(DatV.ApeyNom);
 		printf("\tMatricula: ");
 		scanf("%d",&DatV.Mat);
-		printf("\tDNI");
+		printf("\tDNI: ");
 		scanf("%d",&DatV.DNI);
-		printf("\tTelefono");
+		printf("\tTelefono: ");
 		_flushall();
 		gets(DatV.Tel);
 		fwrite(&DatV,sizeof(DatosVet),1,Vet);
 		fclose(Vet);
 		
-	
-		strcpy(DatU.Nombre,DatV.ApeyNom);
-	    Cargarusuario(DatU);
+	    //Registro de usuario  del veterinario.
+	    
+	    Vet = fopen("Usuario.dat","a+b");
+	    if(Vet==NULL)
+	    {
+	    	printf("Error al abrir el archivo\n");
+	    }
+	    else
+	    {
+	    	strcpy(DatU.Nombre,DatV.ApeyNom);
+		
+		    printf("Registrar nombre de usuario\n");
+		    printf("1-Debe ser unico\n2-Debe comenzar con una letra minuscula\n3-Debe tener como minimo dos letras mayusculas\n");
+		    printf("4-Tener como maximo 3 digitos\n5-Debe tener de 6-10 caracteres\n");
+		    b=0;
+		    while(b==0)
+		    {
+		        printf("Ingrese el nombre de usuario: ");
+		        _flushall();
+	    	    gets(DatU.Usuar);
+			    ComprobarUsuario(DatU.Usuar,b,Vet);
+		    }
+		  
+	       //registro de contraseñia del veterinario
+	  
+	  	    printf("Registrar contraseña:\n");
+		    printf("1- Debe contener como minimo: una letra mayuscula,una minuscula y un numero\n");
+		    printf("2- Solo puede contener caracteres alfanumericos\n");
+		    printf("3- Debe tener entre 6-32 caracteres\n");
+		    printf("4- No debe tener mas de 3 caracteres numericos consecutivos\n");
+		    printf("5- No debe tener 2 letras consecutivas ascendentes\n");
+		    b=0;
+	  	    while(b==0)
+	  	    {
+	  		    printf("Ingrese la contraseña: ");
+			    _flushall();
+			    gets(DatU.Contra);
+			    ComprobarContra(DatU.Contra,b);
+	  	    }
+	  	    
+	  	    fwrite(&DatU,sizeof(Usuario),1,Vet);
+	  	    fclose(Vet);
+		
+	    }
+
 	}
 }
 
 
-int UsuarioRepetido(Usuario DatU)
+
+int UsuarioRepetido(char NomU[10],FILE *&Vet)
 {
 	Usuario Us;
 	int b=1;
-	FILE *Vet;
-	
-	Vet = fopen("Usuario.dat","a+b");
-	if(Vet == NULL)
-	{
-		printf("Error al abrir el archivo\n");
-	}
-	else
-	{
-		rewind(Vet);
-		fread(&Us,sizeof(Usuario),1,Vet);
-	    while(!feof(Vet))
-	    {
-	    	if(strcmp(Us.Usuar,DatU.Usuar)==0)
-	    	{
-	    		b=0;
-	    		return b;
-	    	}
-	    	
-	    	fread(&Us,sizeof(Usuario),1,Vet);
-	    	
-	    }
-	    return b;
-	    fclose(Vet);
-	}
 
-	
-	
+    rewind(Vet);
+	fread(&Us,sizeof(Usuario),1,Vet);
+	while(!feof(Vet))
+	{
+    	if(strcmp(Us.Usuar,NomU)==0)
+    	{
+    		return b=0;
+    	}
+    	
+    	fread(&Us,sizeof(Usuario),1,Vet);
+    	
+    }
+    return b;
+    fclose(Vet);
 }
 
-
-void Cargarusuario(Usuario &DatU)
+void ComprobarUsuario(char NomU[10], int &b,FILE *&Vet)
 {
-	    char aux,aux1;
-	    int Long,b=0,r,cm=0,May=0,Min=0,Num=0;
-	
-		printf("Registrar nombre de usuario\n");
-		printf("1-Debe ser unico\n2-Debe comenzar con una letra minuscula\n3-Debe tener como minimo dos letras mayusculas\n");
-		printf("4-Tener como maximo 3 digitos\n5-Debe tener de 6-10 caracteres");			
-	    while(b==0)
+	    
+	    int Long,r,cm=0;
+	    
+		Long = strlen(NomU);
+		r = UsuarioRepetido(NomU,Vet);
+		if(r==1)
 		{
-			cm=0;
-			printf("Ingrese el nombre de usuario: ");
-		    _flushall();
-		    gets(DatU.Usuar);
-		    Long = strlen(DatU.Usuar);
-		    
-		    r = UsuarioRepetido(DatU);
-		    if(r==1)
-		    {
-		    	if(Long>=6 && Long<=10)
-	            {
-		        	if(islower(DatU.Usuar[0]) != 0)
+		    if(Long>=6 && Long<=10)
+	        {
+		        if(islower(NomU[0]) != 0)
+		        {
+		        	for(int i=0;i<Long;i++)
 		        	{
+		        		if(isupper(NomU[i]) != 0)
+		        		{
+		        			cm++;
+		        		}
+		        	}
+		        	if(cm>=2)
+		        	{
+		        		cm=0;
 		        		for(int i=0;i<Long;i++)
 		        		{
-		        			if(isupper(DatU.Usuar[i]) != 0)
+		        			if(isdigit(NomU[i])!=0)
 		        			{
 		        				cm++;
 		        			}
 		        		}
-		        		if(cm>=2)
-		        		{
-		        			cm=0;
-		        			for(int i=0;i<Long;i++)
-		        			{
-		        				if(isdigit(DatU.Usuar[i])!=0)
-		        				{
-		        					cm++;
-		        				}
-		        			}
-		        			if(cm<=3)
-		        			{
-		        				b=1;
-		        			}
-		        			else
-		        			{
-		        				printf("Error: No puede contener mas de 3 digitos.");
-		        			}
-		        		}
-		        		else
-		        		{
-		        			printf("\nError: Debe tener como minimo 2 letras mayusculas.");
-		        		}
+		        		if(cm<=3)
+		       			{
+		       				b=1;
+		       			}
+		       			else
+		       			{
+		       				printf("\nError: No puede contener mas de 3 digitos.\n");
+		       			}
+		       		}
+	        		else
+	        		{
+						 printf("\nError: Debe tener como minimo 2 letras mayusculas.\n");
+		        	}
 		        		
-		        	}
-		        	else
-		        	{
-		        		printf("\nError: La primera letra no es minuscula.");
-		        	}
-	            }
-	            else
-	            {
-		       	    printf("\nError: Debe ingresar un usuario de 6-10 caracteres.");
-	            }
-		    }
-		    else
-		    {
-		    	printf("\nError: El usuario ya existe.");
-		    }
-	        
+		        }
+		       	else
+		        {
+		        	printf("\nError: La primera letra debe ser minuscula.\n");
+		        }
+	        }
+	        else
+	        {
+		       	printf("\nError: Debe ingresar un usuario de 6-10 caracteres.\n");
+	        }
 		}
-		
-		b=0;
-		
-		printf("Registrar contraseña:\n");
-		printf("1- Debe contener como minimo: una letra mayuscula,una minuscula y un numero\n");
-		printf("2- Solo puede contener caracteres alfanumericos\n");
-		printf("3- Debe tener entre 6-32 caracteres\n");
-		printf("4- No debe tener mas de 3 caracteres numericos consecutivos\n");
-		printf("5- No debe tener 2 letras consecutivas ascendentes\n");
-		
-		while(b==0)
+		else
 		{
-			Num=0;
-			printf("Ingrese la contraseña: ");
-			_flushall();
-			gets(DatU.Contra);
-			Long = strlen(DatU.Contra);
-			if(Long >= 6 && Long <= 32)
+		    printf("\nError: El usuario ya existe.\n");
+		}
+}
+	        
+	
+void ComprobarContra(char ConU[32],int &b)
+{	
+        char aux,aux1;
+        int Long,May=0,Min=0,Num=0;
+			
+		Long = strlen(ConU);
+		if(Long >= 6 && Long <= 32)
+		{
+			for(int i=0;i<Long;i++)
+			{		
+				if(isalnum(ConU[i])==0)
+				{
+					Num=1;
+				}
+			}
+			if(Num==0)	
 			{
 				for(int i=0;i<Long;i++)
 				{
-					
-					if(isalnum(DatU.Contra[i])==0)
+					if(isupper(ConU[i])!=0)
+					{
+						May=1;
+					}
+					else if(islower(ConU[i])!=0)
+					{
+						Min=1;
+					}
+					else if(isdigit(ConU[i])!=0)
 					{
 						Num=1;
 					}
 				}
-				if(Num==0)
+				if(Num==1 && May==1 && Min==1)
 				{
+					Num=0;
+					May=0;
 					for(int i=0;i<Long;i++)
 					{
-						if(isupper(DatU.Contra[i])!=0)
+						if(isdigit(ConU[i])!=0)
 						{
-							May=1;
-						}
-						else if(islower(DatU.Contra[i])!=0)
-						{
-							Min=1;
-						}
-						else if(isdigit(DatU.Contra[i])!=0)
-						{
-							Num=1;
-						}
-					}
-					if(Num==1 && May==1 && Min==1)
-					{
-						Num=0;
-						May=0;
-						for(int i=0;i<Long;i++)
-						{
-							if(isdigit(DatU.Contra[i])!=0)
+							Num++;
+							if(Num==4)
 							{
-								Num++;
-								if(Num==4)
-								{
-									May=1;
-									i=Long;
-								}
-							}
-							else if(isalpha(DatU.Contra[i])!=0)
-							{
-								Num=0;
+								May=1;
+								i=Long;
 							}
 						}
-						if(May==0)
+						else if(isalpha(ConU[i])!=0)
 						{
 							Num=0;
-							May=0;
-							for(int i=0;i<Long;i++)
-							{
-								if(isalpha(DatU.Contra[i])!=0)
-								{
-									Num++;
-									if(Num==2)
-									{
-										aux = toupper(DatU.Contra[i]);
-										aux1 = toupper(DatU.Contra[i-1]);
-										if(aux>aux1)
-										{
-											May=1;
-											i=Long;
-										}
-									}
-								}
-								else
-							    {
-							    	Num=0;
-							    }
-						    }
-						    if(May==0)
-						    {
-						    	b=1;
-						    }
-						    else
-						    {
-						    	printf("Error: No debe ingresar dos letras consecutivas y ascendentes.");
-						    }
-						}	
-						else
-						{
-							printf("Error: No debe ingresar mas de 3 digitos seguidos.");
 						}
 					}
+					if(May==0)
+					{
+						Num=0;
+						for(int i=0;i<Long;i++)
+						{
+							if(isalpha(ConU[i])!=0)
+							{
+								Num++;
+								if(Num==2)
+								{
+									
+									aux = toupper(ConU[i]);
+									aux1 = toupper(ConU[i-1]);
+									
+									if(aux1 == aux-1)
+									{
+									   May=1;
+									   i=Long;	
+									}
+									
+								}
+							}
+							else
+						    {
+						    	Num=0;
+						    }
+					    }
+					    if(May==0)
+					    {
+					    	b=1;
+					    }
+					    else
+					    {
+					    	printf("\nError: No debe ingresar dos letras consecutivas y ascendentes.\n");
+					    }
+					}	
 					else
 					{
-						printf("Error: Debe ingresar al menos una mayúscula, una minúscula y un número.");
+						printf("\nError: No debe ingresar mas de 3 digitos seguidos.\n");
 					}
-					
 				}
 				else
 				{
-					printf("Error: Debe ingresar caractéres alfanumericos.");
+					printf("\nError: Debe ingresar al menos una mayúscula, una minúscula y un número.\n");
 				}
+				
 			}
 			else
 			{
-				printf("Error: Debe ingresar una contrasenia de 6-32 caractéres.");
+				printf("\nError: Debe ingresar caractéres alfanumericos.\n");
 			}
-			
-			
-			
-			
 		}
-		
-		
+		else
+		{
+			printf("\nError: Debe ingresar una contrasenia de 6-32 caractéres.\n");
+		}
+					
 }
 
 
