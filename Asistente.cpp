995b74cdc6169541c70a27464bdbe077de343e56
FrancoLamagni <FrancoLamagni@outlook.es>
2020-12-10 20:30:57 -0300

    Modificacion de la funcion RegistrarMas de Asistente.cpp,Revisar si esta registrada la mascota

--- Asistente.cpp
+++ Asistente.cpp
@@ -77,49 +77,105 @@ main()
 	
 }
 
+
 void RegistrarMas(FILE *Us)
 {
-	Mascotas DatM;
+	Mascotas DatM,Aux;
+	int DNIRep,band=0;
+	char NomRep[60];
 	
-	Us = fopen("Mascotas.dat","ab");
+	Us = fopen("Mascotas.dat","a+b");
 	if(Us == NULL)
 	{
 		printf("Error: No se pudo abrir el archivo\n");
 	}
 	else
 	{
+		
 		printf("Ingrese los siguientes datos de la mascota:\n\n");
 		
 		printf("\tNombre y apellido de la mascota: ");
 		_flushall();
-		gets(DatM.Nombre);
-		
-		printf("\tDomicilio: ");
-		_flushall();
-		gets(DatM.Dom);
+		gets(NomRep);
+		strupr(NomRep);
 		
 		printf("\tDNI del dueño: ");
-		scanf("%d",&DatM.DNID);
+		scanf("%d",&DNIRep);
 		
-		printf("\tLocalidad: ");
-		_flushall();
-		gets(DatM.Loc);
+		rewind(Us);
+		fread(&Aux,sizeof(Mascotas),1,Us);
+		while(!feof(Us))
+		{
+			strupr(Aux.Nombre);
+			
+			if(Aux.DNID==DNIRep)
+			{
+			   	if(strcmp(Aux.Nombre,NomRep)==0)
+			   	{
+			   		printf("La mascota ya esta registrada.\n1-Actualizar datos\n2-No Actualizar\n\nIngrese una opcion: ");
+			   		scanf("%d",&band);
+			   		if(band==1)
+			   		{
+			   				printf("\tDomicilio: ");
+							_flushall();
+							gets(DatM.Dom);
 		
-		printf("\tFecha de nacimiento (DD/MM/AAAA): ");
-		scanf("%d/%d/%d",&DatM.fecNac.dia,&DatM.fecNac.mes,&DatM.fecNac.anio);
+							printf("\tLocalidad: ");
+							_flushall();
+							gets(DatM.Loc);
+			   			
+			   				printf("\tPeso de la mascota: ");
+							scanf("%f",&DatM.Peso);
 		
-		printf("\tPeso de la mascota: ");
-		scanf("%f",&DatM.Peso);
+							printf("\tTelefono: ");
+							_flushall();
+							gets(DatM.Tel);
+							
+							fseek(Us,-sizeof(Mascotas),1);
+							fwrite(&DatM,sizeof(Mascotas),1,Us);
+							
+							printf("\nDatos actualizados con exito.\n");
+							fseek(Us,0,2);
+			   		}
+			   		
+			   	}
+			   	
+			}
+			
+		    fread(&Aux,sizeof(Mascotas),1,Us);	
+		}
 		
-		printf("\tTelefono: ");
-		_flushall();
-		gets(DatM.Tel);
 		
-		fwrite(&DatM,sizeof(Mascotas),1,Us);
+		if(band==0)
+		{
+			strcpy(DatM.Nombre,NomRep);
+			DatM.DNID=DNIRep;
+			
+			printf("\tDomicilio: ");
+			_flushall();
+			gets(DatM.Dom);
+		
+			printf("\tLocalidad: ");
+			_flushall();
+			gets(DatM.Loc);
+			
+			printf("\tFecha de nacimiento (DD/MM/AAAA): ");
+			scanf("%d/%d/%d",&DatM.fecNac.dia,&DatM.fecNac.mes,&DatM.fecNac.anio);
+			
+			printf("\tPeso de la mascota: ");
+			scanf("%f",&DatM.Peso);
+		
+			printf("\tTelefono: ");
+			_flushall();
+			gets(DatM.Tel);
+		
+			fwrite(&DatM,sizeof(Mascotas),1,Us);
+		}
+
+		
 		
 	}
 	fclose(Us);
 }
-
 
 
